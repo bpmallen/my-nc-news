bd639d49d6ac324b79de2bfabf4d9327327cccfc
bpmallen <bpmallen@gmx.com>
2024-11-25 11:10:45 +0000

    2-get-api

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,18 @@
 const endpointsJson = require("../endpoints.json");
+const app = require("../app");
+const request = require("supertest");
+const seed = require("../db/seeds/seed");
+const testData = require("../db/data/test-data");
+const db = require("../db/connection");
 /* Set up your test imports here */
+
+beforeEach(() => {
+  return seed(testData);
+});
+
+afterAll(() => {
+  return db.end();
+});
 
 /* Set up your beforeEach & afterAll functions here */
 
