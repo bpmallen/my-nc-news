aebbe483c2d731a6d5c3476a24dd16df52415562
bpmallen <bpmallen@gmx.com>
2025-01-28 14:49:42 +0000

    PORT for render hosting fix 1

--- listen.js
+++ listen.js
@@ -1,6 +1,8 @@
 const app = require("./app");
 const { PORT = 9090 } = process.env;
 
-app.listen(PORT, () =>
-  console.log(`Listening on http://localhost:${PORT}/api`)
-);
+// app.listen(PORT, () =>
+//   console.log(`Listening on http://localhost:${PORT}/api`)
+// );
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
